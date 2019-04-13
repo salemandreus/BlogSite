f95293595e50a6a6afb9447bdab3127334d8013e
Salem <salemandreus@gmail.com>
2019-04-13 14:09:54 +0200

    BLOG APP: Added blog sample data which renders on home page, and title awareness to both templates

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'JohnnyB',
+        'title': 'Blog Post 1',
+        'content': 'Bla Bla Bla Bla',
+        'date_posted': '26 May 2019'
+    },
+    {
+        'author': 'Angela',
+        'title': 'Blog Post 2',
+        'content': 'More Bla Bla Bla Bla',
+        'date_posted': '31 May 2019'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html', {'title': 'About'})
